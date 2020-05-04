75f401b245b74745c8b396c8364002f59f1d6433
dimitrisbro <dimitris.brodimas@gmail.com>
2020-05-04 12:10:46 +0300

    new commit

---
+++ preCode.py
@@ -0,0 +1,46 @@
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+
+rawData = pd.read_csv('/home/db/Documents/dipl/data.csv')
+pData = pd.DataFrame()
+pData['FLAG'] = rawData['FLAG']
+pData['CONS_NO'] = rawData['CONS_NO']
+data = rawData.drop(['FLAG', 'CONS_NO'], axis=1)
+
+dIndex = data[data.duplicated()].index # duplicates drop
+data = data.drop(dIndex, axis=0)
+pData = pData.drop(dIndex, axis=0)
+
+zIndex = data[(data.sum(axis=1) == 0)].index  # zero rows drop
+data = data.drop(zIndex, axis=0)
+pData = pData.drop(zIndex, axis=0)
+
+data.columns = pd.to_datetime(data.columns)  # columns reindexing according to dates
+data = data.reindex(sorted(data.columns), axis=1)
+cols = data.columns
+
+data.reset_index(inplace=True, drop=True)  # index sorting
+pData.reset_index(inplace=True, drop=True)
+
+
+data = data.interpolate(method='linear', limit=2,  # filling NaN values
+                        limit_direction='both', axis=0).fillna(0)
+
+for i in range(data.shape[0]):  # outliers treatment
+    m = data.loc[i].mean()
+    st = data.loc[i].std()
+    data.loc[i] = data.loc[i].mask(data.loc[i] > (m + 3 * st), other=m + 3 * st)
+
+#data.to_csv(r'/home/db/Documents/dipl/visualization.csv', index=False, header=True)
+
+scale = MinMaxScaler()  # Scaling
+scale.fit(data)
+nData = pd.DataFrame(scale.transform(data))
+nData.columns = cols
+print(nData)
+
+
+preprData = pd.concat([pData, nData], axis=1, sort=False) # Back to initial format
+
+print(preprData)
+#preprData.to_csv(r'/home/db/Documents/dipl/preprocessed.csv', index=False, header=True)
--- visualization.py
+++ visualization.py
@@ -1,14 +1,12 @@
 import pandas as pd
+import numpy as np
 from matplotlib import pyplot as plt
 
-rawData1 = pd.read_csv('/home/db/Documents/dipl/export_dataframe12.csv', nrows=3)
+rawData1 = pd.read_csv('/home/db/Documents/dipl/visualization.csv', nrows=3)
 cols = rawData1.columns
-rawData2 = pd.read_csv('/home/db/Documents/dipl/export_dataframe12.csv', skiprows=40254)
+rawData2 = pd.read_csv('/home/db/Documents/dipl/visualization.csv', skiprows=40254)
 rawData2.columns = cols
-rawData = pd.concat([rawData1, rawData2], ignore_index=True)
-data = rawData.drop(['FLAG', 'CONS_NO'], axis=1).fillna(0)
-data.columns = pd.to_datetime(data.columns)
-data = data.reindex(sorted(data.columns), axis=1)
+data = pd.concat([rawData1, rawData2], ignore_index=True)
 
 fig, axs = plt.subplots(2, 1)
 fig.suptitle('Consumers With Fraud', fontsize=18)
@@ -19,11 +17,10 @@ axs[0].set_title('Consumer 0', fontsize=16)
 axs[0].set_xlabel('Dates of Consumption')
 axs[0].set_ylabel('Consumption')
 
-data.loc[1].plot(ax=axs[1], color='firebrick', grid=True)
+data.loc[2].plot(ax=axs[1], color='firebrick', grid=True)
 axs[1].set_title('Consumer 1', fontsize=16)
 axs[1].set_xlabel('Dates of Consumption')
 axs[1].set_ylabel('Consumption')
-
 
 fig, axs = plt.subplots(2, 1)
 fig.suptitle('Consumers Without Fraud', fontsize=18)
@@ -38,7 +35,6 @@ data.loc[4].plot(ax=axs[1], color='teal', grid=True)
 axs[1].set_title('Consumer 40256', fontsize=16)
 axs[1].set_xlabel('Dates of Consumption')
 axs[1].set_ylabel('Consumption')
-
 
 fig2, axs2 = plt.subplots(2, 2)
 fig2.suptitle('Statistics for Consumers without Fraud', fontsize=18)
@@ -63,7 +59,6 @@ data.loc[0].describe().drop(['count']).plot(kind='bar', ax=axs2[1, 1], color='fi
 axs2[1, 1].set_title('Statistics', fontsize=16)
 axs2[1, 1].set_ylabel('Values')
 
-
 fig3, axs3 = plt.subplots(2, 2)
 fig3.suptitle('Statistics for Consumers without Fraud', fontsize=18)
 plt.subplots_adjust(hspace=0.8)
@@ -86,24 +81,47 @@ data.loc[4].describe().drop(['count']).plot(kind='bar', ax=axs3[1, 1], color='te
 axs3[1, 1].set_title('Statistics', fontsize=16)
 axs3[1, 1].set_ylabel('Values')
 
-
 fig4, axs4 = plt.subplots(2, 1)
 fig4.suptitle('Four Week Consumption', fontsize=16)
 plt.subplots_adjust(hspace=0.5)
 
-for i in range(59,83,7):
-    axs4[0].plot(data.iloc[0,i:i+7].to_numpy(),marker='>',linestyle='-',label='$week {i}$'.format(i=(i%58)%6))
+for i in range(59, 83, 7):
+    axs4[0].plot(data.iloc[0, i:i + 7].to_numpy(), marker='>', linestyle='-', label='$week {i}$'.format(i=(i % 58) % 6))
 axs4[0].legend(loc='best')
 axs4[0].set_title('Without Fraud', fontsize=14)
 axs4[0].set_ylabel('Consumption')
 axs4[0].grid(True)
 
-for i in range(59,83,7):
-    axs4[1].plot(data.iloc[4,i:i+7].to_numpy(),marker='>',linestyle='-',label='$week {i}$'.format(i=(i%58)%6))
+for i in range(59, 83, 7):
+    axs4[1].plot(data.iloc[4, i:i + 7].to_numpy(), marker='>', linestyle='-', label='$week {i}$'.format(i=(i % 58) % 6))
 axs4[1].legend(loc='best')
 axs4[1].set_title('With Fraud', fontsize=14)
 axs4[1].set_ylabel('Consumption')
 axs4[1].grid(True)
 
-#plt.close('all')
+fig5, axs5 = plt.subplots(1, 2)
+a = []
+for i in range(59, 83, 7):
+    a.append(data.iloc[0, i:i + 7].to_numpy())
+cor = pd.DataFrame(a).transpose().corr()
+cax = axs5[0].matshow(cor)
+for (i, j), z in np.ndenumerate(cor):
+    axs5[0].text(j, i, '{:0.1f}'.format(z), ha='center', va='center', color='white')
+alpha = ['week 1', 'week 2', 'week 3', 'week 4']
+axs5[0].set_xticklabels([''] + alpha)
+axs5[0].set_yticklabels([''] + alpha)
+axs5[0].set_title('Customer without Fraud', fontsize=16)
+
+a = []
+for i in range(59, 83, 7):
+    a.append(data.iloc[4, i:i + 7].to_numpy())
+cor = pd.DataFrame(a).transpose().corr()
+cax = axs5[1].matshow(cor)
+for (i, j), z in np.ndenumerate(cor):
+    axs5[1].text(j, i, '{:0.1f}'.format(z), ha='center', va='center', color='white')
+axs5[1].set_xticklabels([''] + alpha)
+axs5[1].set_yticklabels([''] + alpha)
+axs5[1].set_title('Customer with Fraud', fontsize=16)
+fig5.colorbar(cax)
+# plt.close('all')
 plt.show()
